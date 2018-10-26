2e9151c8636b870d8a9d57df8d9c768042a32bd4
XXdreamm <44492150+XXdreamm@users.noreply.github.com>
2018-10-26 16:33:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -353,7 +353,6 @@ client.user.setGame(`1play | Last Music`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 client.on('ready', () => {
- if(msg.content === "1join") {
 var x = client.channels.get("505369701862604825");
 if (x) x.join();
 });
